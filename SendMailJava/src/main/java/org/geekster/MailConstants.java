7d4aeaf1b7359678e2821c081a8c2459311d8ed5
srushtidafe <100861585+srushtidafe@users.noreply.github.com>
2023-06-11 13:07:17 +0530

    Maven project sending the mail using java

---
+++ SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,16 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "srushtidafe2000@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS= "shreyadafe1998@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 11--06-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+
+}
